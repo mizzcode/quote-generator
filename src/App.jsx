001c6ff9608434cc1ebd8519930ed38fe632493a
Misbahudin <misbahudin.id@gmail.com>
2024-06-12 00:42:55 +0700

    style: remove background unnecessary

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,14 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className='dark:bg-oldBlue dark:text-white text-center flex items-center flex-col space-y-14 mt-28'>
+      <div className='dark:text-white text-center flex items-center flex-col space-y-14 mt-28'>
         <h1 className='font-bold text-3xl'>Quote Generator</h1>
         <p className='max-w-sm md:max-w-3xl lg:max-w-4xl'>
           {quote ? quote : 'Click the button below to generate a quote'}
         </p>
         <button
           onClick={handleQuote}
-          className='font-semibold rounded-full bg-blue-400 hover:bg-blue-500 px-5 py-2 mt-10'>
+          className='font-semibold rounded-full bg-blue-500 hover:bg-blue-600 px-5 py-2 mt-10'>
           Generate
         </button>
       </div>
